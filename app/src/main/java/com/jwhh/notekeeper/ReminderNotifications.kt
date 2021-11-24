8aaa39f3af10af8d2d217f7f30b4e43cab776bdf
legenduzo <precious@evolvedigital.org>
2021-11-24 15:30:09 +0100

    Updated Notifications to take advantage of backstack when the intent opens a specific page

--- app/src/main/java/com/jwhh/notekeeper/ReminderNotifications.kt
+++ app/src/main/java/com/jwhh/notekeeper/ReminderNotifications.kt
@@ -4,8 +4,10 @@ import android.annotation.TargetApi
 import android.app.Notification
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.getIntent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 
@@ -13,7 +15,14 @@ object ReminderNotifications {
     private const val NOTIFICATION_TAG = "Reminder"
     const val REMINDER_CHANNEL = "reminders"
 
-    fun notify (context: Context, titleText: String, noteText:String, number: Int) {
+    fun notify (context: Context, titleText: String, noteText:String, notePosition: Int) {
+
+        val intent = Intent(context, NoteActivity::class.java)
+        intent.putExtra(NOTE_POSITION, notePosition)
+
+        val pendingIntent = TaskStackBuilder.create(context)
+            .addNextIntentWithParentStack(intent)
+            .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
 
         val shareIntent = PendingIntent.getActivity(context, 0,
         Intent.createChooser(Intent(Intent.ACTION_SEND)
@@ -30,7 +39,7 @@ object ReminderNotifications {
             .setContentText(noteText)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setAutoCancel(true)
-
+            .setContentIntent(pendingIntent)
             .addAction(R.drawable.ic_menu_share, "Share", shareIntent)
 
         notify(context,builder.build())
